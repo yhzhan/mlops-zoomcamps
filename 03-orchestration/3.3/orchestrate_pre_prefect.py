8da38e4b3c7e1b4b010616a3cd134fc76cdee10e
Wzhiye <632839407@qq.com>
2025-06-09 11:36:09 +0200

    update the course note of homework03 for prefect

---
+++ 03-orchestration/3.3/orchestrate_pre_prefect.py
@@ -0,0 +1,125 @@
+import pathlib
+import pickle
+import pandas as pd
+import numpy as np
+import scipy
+import sklearn
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import root_mean_squared_error
+from scipy.sparse import csr_matrix
+import mlflow
+import xgboost as xgb
+from prefect import flow, task
+
+@task(retries=3, retry_delay_seconds=10, log_prints=True)
+def read_data(filename: str) -> pd.DataFrame:
+    """Read data into DataFrame"""
+    df = pd.read_parquet(filename)
+
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+
+    df["duration"] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ["PULocationID", "DOLocationID"]
+    df[categorical] = df[categorical].astype(str)
+
+    return df 
+
+@task(log_prints=True)
+def add_features(
+    df_train: pd.DataFrame, df_val: pd.DataFrame
+) -> tuple[csr_matrix, csr_matrix, np.ndarray, np.ndarray, DictVectorizer]:
+    """Add features to the model"""
+    df_train["PU_DO"] = df_train["PULocationID"] + "_" + df_train["DOLocationID"]
+    df_val["PU_DO"] = df_val["PULocationID"] + "_" + df_val["DOLocationID"]
+
+    categorical = ["PU_DO"]  #'PULocationID', 'DOLocationID']
+    numerical = ["trip_distance"]
+
+    dv = DictVectorizer()
+
+    train_dicts = df_train[categorical + numerical].to_dict(orient="records")
+    X_train = dv.fit_transform(train_dicts)
+
+    val_dicts = df_val[categorical + numerical].to_dict(orient="records")
+    X_val = dv.transform(val_dicts)
+
+    y_train = df_train["duration"].values
+    y_val = df_val["duration"].values
+    return X_train, X_val, y_train, y_val, dv
+
+@task(log_prints=True)
+def train_best_model(
+    X_train: scipy.sparse._csr.csr_matrix,
+    X_val: scipy.sparse._csr.csr_matrix,
+    y_train: np.ndarray,
+    y_val: np.ndarray,
+    dv: sklearn.feature_extraction.DictVectorizer,
+) -> None:
+    """train a model with best hyperparams and write everything out"""
+
+    with mlflow.start_run():
+        train = xgb.DMatrix(X_train, label=y_train)
+        valid = xgb.DMatrix(X_val, label=y_val)
+
+        best_params = {
+            "learning_rate": 0.09585355369315604,
+            "max_depth": 30,
+            "min_child_weight": 1.060597050922164,
+            "objective": "reg:linear",
+            "reg_alpha": 0.018060244040060163,
+            "reg_lambda": 0.011658731377413597,
+            "seed": 42,
+        }
+
+        mlflow.log_params(best_params)
+
+        booster = xgb.train(
+            params=best_params,
+            dtrain=train,
+            num_boost_round=100,
+            evals=[(valid, "validation")],
+            early_stopping_rounds=20,
+        )
+
+        y_pred = booster.predict(valid)
+        rmse = root_mean_squared_error(y_val, y_pred)
+        mlflow.log_metric("rmse", rmse)
+
+        pathlib.Path("models").mkdir(exist_ok=True)
+        with open("models/preprocessor.b", "wb") as f_out:
+            pickle.dump(dv, f_out)
+        mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
+
+        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+    return None
+
+
+@flow
+def main_flow(
+    train_path: str = "./data/green_tripdata_2021-01.parquet",
+    val_path: str = "./data/green_tripdata_2021-02.parquet",
+) -> None:
+    """The main training pipeline"""
+
+    # MLflow settings
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("nyc-taxi-experiment")
+
+    # Load
+    df_train = read_data(train_path)
+    df_val = read_data(val_path)
+
+    # Transform
+    X_train, X_val, y_train, y_val, dv = add_features(df_train, df_val)
+
+    # Train
+    train_best_model(X_train, X_val, y_train, y_val, dv)
+
+
+if __name__ == "__main__":
+    main_flow()
